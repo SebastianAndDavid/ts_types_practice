36bdc2df39be5d6312c94fd7c94834ddde106d4f
David <Rigby335@hotmail.com>
2023-09-12 23:50:00 -0700

    useState and click event w/ boolean type in interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import Messages from "./components/Messages";
 
 function App() {
+  const [login, setLogin] = useState(false);
   return (
     <>
-      <Messages message=" Hello and good morning" count={5} />
+      <button onClick={() => setLogin(true)}>Login</button>
+      <Messages
+        message=" Hello and good morning"
+        count={5}
+        isLoggedIn={login}
+      />
     </>
   );
 }
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,12 +1,19 @@
 interface MessageProps {
   message: string;
   count: number;
+  isLoggedIn: boolean;
 }
 export default function Messages(props: MessageProps) {
   return (
     <div>
-      Hello. You have {props.count} messages. Your first message says -
-      {props.message}
+      {props.isLoggedIn ? (
+        <div>
+          Hello. You have {props.count} messages. Your first message says -
+          {props.message}
+        </div>
+      ) : (
+        <div>Please log in to see your messages</div>
+      )}
     </div>
   );
 }
